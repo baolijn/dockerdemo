92caf03c48e9c5a42456ec4e52a0482121451c0d
baoli <libao_fighting@outlook.com>
2017-04-26 10:05:31 +0800

    testdocker

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! Iff been seen {} times.\n'.format(count)
+    return 'Hello docker! If been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
